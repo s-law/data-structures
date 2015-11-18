17dd381db485a87e74f992f0b00bbfac5b32dc08
s-law <slaw@nuclearanvil.com>
2015-11-18 15:28:34 -0800

    Implement functional queue data structure to pass all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,8 @@
 var Queue = function() {
-  var someInstance = {};
+  var someInstance = {
+    queueSize: 0,
+    lowest: 0
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +10,22 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.lowest + someInstance.queueSize] = value;
+    someInstance.queueSize++;
   };
 
   someInstance.dequeue = function() {
+    var item = storage[someInstance.lowest];
+
+    delete storage[someInstance.lowest];
+    someInstance.queueSize--;
+    someInstance.lowest++;
+
+    return item;
   };
 
   someInstance.size = function() {
+    return someInstance.queueSize >= 0 ? someInstance.queueSize : 0;
   };
 
   return someInstance;
