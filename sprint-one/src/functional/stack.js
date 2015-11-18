2a8055fc9c3255c2d0ef0e376ab16b95294f33b9
s-law <slaw@nuclearanvil.com>
2015-11-18 15:03:27 -0800

    Implement basic stack in functional pattern to pass all tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,29 @@
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {
+    stackSize : 0
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.stackSize] = value;
+    someInstance.stackSize++;
   };
 
   someInstance.pop = function() {
+    var result;
+
+    result = storage[someInstance.stackSize - 1];
+    delete storage[someInstance.stackSize - 1];
+    someInstance.stackSize--;
+
+    return result;
   };
 
   someInstance.size = function() {
+    return someInstance.stackSize >= 0 ? someInstance.stackSize : 0;
   };
 
   return someInstance;
