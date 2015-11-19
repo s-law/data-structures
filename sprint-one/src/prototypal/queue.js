287867aa59bcaa212f2bb65bd36d06e22b3ba520
s-law <slaw@nuclearanvil.com>
2015-11-19 10:05:36 -0800

    Completed prototypal queue to pass test cases. Noted difference between previous 2 instantiation types

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var storage = Object.create(queueMethods);
+  storage.queueSize = 0;
+  storage.lowest = 0;
+  return storage;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this[this.queueSize + this.lowest] = value;
+  this.queueSize++;
+};
 
+queueMethods.dequeue = function() {
+  var item = this[this.lowest];
+
+  delete this[this.lowest];
+  this.lowest++;
+  this.queueSize--;
+  
+  return item;
+};
+
+queueMethods.size = function() {
+  return this.queueSize >= 0 ? this.queueSize : 0;
+};
