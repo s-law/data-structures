62b2c920a2bf59ffa39b0a56874677c4519ee36a
s-law <slaw@nuclearanvil.com>
2015-11-18 16:19:08 -0800

    Implemented queue & stack in functional-shared methodology. Passing all tests. Noted differences with functional instances

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var storage = {
+    queueSize: 0,
+    lowest: 0
+  };
+
+  _.extend(storage, queueMethods);
+  return storage;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this[this.queueSize + this.lowest] = value;
+    this.queueSize++;
+  },
+
+  dequeue: function() {
+    var item = this[this.lowest];
+
+    delete this[this.lowest];
+    this.lowest++;
+    this.queueSize--;
+
+    return item;
+  },
+
+  size: function() {
+    return this.queueSize >= 0 ? this.queueSize : 0;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  //storage is THIS
+  var storage = {};
+  storage.stackSize = 0;
+
+  //This extend method is vital to the working of functional-shared stacks
+  //unlike prototypical instantiation we must explicitly extend. 
+  _.extend(storage, stackMethods);
+
+  return storage;
 };
 
+
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this[this.stackSize] = value;
+  this.stackSize++;
+};
+
+stackMethods.pop = function() {
+  var item = this[this.stackSize - 1];
+  delete this[this.stackSize - 1];
+  this.stackSize--; 
+
+  return item;
+};
+
+stackMethods.size = function() {
+  return this.stackSize >= 0 ? this.stackSize : 0;
+};
 
 
