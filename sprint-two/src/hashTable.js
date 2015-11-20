a0c3d1617e2a7fc1bb24f3e0d8bec5d4a3c51fae
s-law <slaw@nuclearanvil.com>
2015-11-19 20:23:51 -0800

    Refactored retrieve method to discard unnecessary check. Also noted time complexity of all methods.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,6 +5,7 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
+//insert method requires O(1) - hashing is O(n) where n is string size
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
@@ -13,22 +14,17 @@ HashTable.prototype.insert = function(k, v) {
   this._storage.set(index, obj);
 };
 
+//O(1)
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage.get(index).hasOwnProperty(k)) {
-    return this._storage.get(index)[k];  
-  }
-  return undefined;
+  return this._storage.get(index)[k];
 };
 
+//O(1)
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var obj = this._storage.get(index);
   delete obj[k];
-
-  if(Object.keys(this._storage.get(index)).length === 0){
-    this._storage.set(index, undefined);
-  }
 };
 
 
