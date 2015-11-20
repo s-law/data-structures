eb3460101fbca1ca75798391c65d7310c6007d87
s-law <slaw@nuclearanvil.com>
2015-11-19 17:38:50 -0800

    Implementing a hash table in JavaScript (WIP).

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,39 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) === undefined) {
+    // if (this._storage.get(index)[k] === k) {
+      var obj = {};
+      obj[k] = v;
+      this._storage.set(index, obj);
+    // }
+  } else if (this._storage.get(index).hasOwnProperty(k)) {
+    var obj = {};
+    obj[k] = v;
+    this._storage.set(index, obj);
+  } else {
+    do {
+      index++;
+      index = index % this._limit;
+      console.log(index);
+    } while (this._storage.get(index) !== undefined /*|| !this._storage.get(index).hasOwnProperty(k)*/);
+    var obj = {};
+    obj[k] = v;
+    this._storage.set(index, obj);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index).hasOwnProperty(k)) {
+    return this._storage.get(index)[k];  
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
